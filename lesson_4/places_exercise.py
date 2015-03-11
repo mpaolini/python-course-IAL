5b80d284d57bffb6d27b0b979a88f0e10dbff154
Marco Paolini <marco@credra.com>
2015-03-11 12:24:08 +0100

    Solution for the places exercise

--- lesson_4/places_exercise.py
+++ lesson_4/places_exercise.py
@@ -1,42 +1,32 @@
 '''
-# HEY
-# Google places API exercise
-
-# Places in vicinity
-
-1. Build a function that given a string, returns all places that contain the
-string
-
->>> find_places('IAL')
-[('IAL', 12.5, 41.95)]
-
-
-2. Build a function that given a place, returns a list of places nearby
-
->> > nearby_places({})
-[{'name': 'ciao'}]
-
-3. Get place name from command line argument
-
-> >> import subprocess
-> >> subprocess.check_output(['places_exercise.py', 'IAL'])
-ciao
+Build a function that given a string, returns all places that contain the string:
+>>> places = find_places('IAL')
+>>> places[0]
+('IAL ROMA E LAZIO', 41.954964, 12.523495)
 '''
 
 import urllib.request
 import urllib.parse
 import json
-import pprint
 
 
 def find_places(place_name):
     KEY = 'AIzaSyBnTJ0P-nXBgUEut5z_EZYQVwudWl4lP1E'
-    qs = urllib.parse.urlencode({'query': place_name, 'key': KEY, 'sensor': 'false'})
+    qs = urllib.parse.urlencode(
+        {'query': place_name,
+         'key': KEY,
+         'sensor': 'false'})
     url = 'https://maps.googleapis.com/maps/api/place/textsearch/json?' + qs
     resp = urllib.request.urlopen(url).read()
     data = json.loads(resp.decode())
-    pprint.pprint(data)
-    return [res['geometry']['location'] for res in data['results']]
+    places = []
+    for place in data['results']:
+        name = place['name']
+        lat = place['geometry']['location']['lat']
+        lng = place['geometry']['location']['lng']
+        my_tuple = (name, lat, lng)
+        places.append(my_tuple)
+    return places
 
 
 def nearby_place(lat, lng):
