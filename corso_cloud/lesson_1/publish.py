5f6e5e1d5be62a86b9923a53c193b475cd6b75a0
Marco Paolini <marco@credra.com>
2015-05-28 14:10:56 +0200

    Publish on s3

--- corso_cloud/lesson_1/publish.py
+++ corso_cloud/lesson_1/publish.py
@@ -1,11 +1,28 @@
 '''
 Copies the archive into a shared dropbox folder
 '''
-import shutil
 import glob
-import os.path
+import botocore.session
 
 
-for fname in glob.glob('dist/twitter_example-*.tar.gz'):
-    shutil.copy(fname, os.path.expanduser('~/Dropbox/'))
-    print('copying {} into Dropbox'.format(fname))
+def publish():
+    bucket = 'tsac_marco_test'
+    session = botocore.session.Session(
+        session_vars={'profile': (None, None, 'tsac-test')}
+    )
+    s3 = session.create_client('s3')
+    s3.create_bucket(Bucket=bucket)
+
+    # get archives
+    for fname in glob.glob('dist/twitter_example-*.tar.gz'):
+        with open(fname, 'rb') as f:
+            s3.put_object(
+                Bucket=bucket,
+                Key='archive.tar.gz',
+                Body=f,
+                ACL='public-read'
+            )
+        print('archive available at https://s3.amazonaws.com/{}/archive.tar.gz'.format(bucket))
+
+if __name__ == '__main__':
+    publish()
